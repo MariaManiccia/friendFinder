1e1ea8ece152f7b9d99a238bd041ca2282ed0aa8
Maniccia <mariamaniccia@gmail.com>
2019-02-12 15:36:39 -0500

    what?

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,12 +6,12 @@ var path = require("path");
 module.exports = function (app) {
 
     // Making an html GET request, directing it to the right html page
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
-    });
+    app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/survey.html"));
+	});
 
-    // Setting the default to the home page
-    app.get("/home", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });
+	// default route for homepage
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	});
 };
