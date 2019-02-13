96543cf314c3c72d2e7807a0b593be79b0ccb524
Maniccia <mariamaniccia@gmail.com>
2019-02-12 22:34:31 -0500

    Maybe

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,31 @@
 
 var friendArray = [
     
-{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+	{
+		name: "Nicki",
+		photo: "https://pixel.nymag.com/imgs/daily/vulture/2019/02/11/11-nicki-minaj.w330.h330.jpg",
+		scores: [3, 2, 4, 2, 4, 2, 2, 3, 5, 5]
+	},
+	{
+		name: "Bey",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNJ9IsLhJvBYGBynHn9PJFCIihbY9lrs9uCNwLQOL8y4gCka2Z",
+		scores: [2, 3, 5, 4, 2, 1, 2, 3, 4, 2]
+	},
+	{
+		name: "Cardi",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSqOepgSmA2TnX2RedCcVT-6dP9RSxocIlk2KJ8gOgKfdqniY28Q",
+		scores: [5, 4, 3, 2, 5, 1, 5, 3, 1, 3]
+	},
+	{
+		name: "Jay",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4e3jVhqZQUC6wgXSle3oYcI_FmOsR0DKjKI4KW0TfwYj6P2e4",
+		scores: [2, 3, 3, 5, 3, 4, 1, 5, 2, 3]
+	},
+	{
+		name: "Offset",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Q8Z3yWLPWLGq8wjHknw-5WJNtaI9r0P3G7c0OYURZK_Sy2Y",
+		scores: [1, 4, 5, 4, 1, 4, 4, 2, 3, 1]
+	}
 
   ];
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,47 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var friendsArray = require("../data/friends");
 
+module.exports = function(app) {
 
-module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
+  // Return all friends
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsArray);
+  });
 
-    app.get("/api/friends", function (req, res) {
-        res.json(friendsArray);
-    });
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
 
-    app.post("/api/friends", function (req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        res.json(friendsArray);
-    });
+    // Receive friends stats
+    var user = req.body;
 
+    // parseInt for scores
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    }
+
+    // default friend stats
+    var friendIndex = 0;
+    var minDiff = 40;
+
+    //  comparing the score
+    //  whatever the difference is, add to the total difference
+    for(var i = 0; i < friendsArray.length; i++) {
+      var totalDiff = 0;
+      for(var j = 0; j < friendsArray[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friendsArray[i].scores[j]);
+        totalDiff += difference;
+      }
+
+      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      if(totalDiff < minDiff) {
+        friendIndex = i;
+        minDiff = totalDiff;
+      }
+    }
+
+    // add user the friend array
+    friendsArray.push(user);
+
+    // send the match
+    res.json(friendsArray[friendIndex]);
+  });
 };
